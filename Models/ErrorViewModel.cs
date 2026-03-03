4e236cbe12946557206da11c18e78c22aa2555ef
Luis Silva <luissilvam537@gmail.com>
2026-03-03 18:18:27 -0300

    Core: Creation of the basic structure of the application.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace SupportManagement.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
